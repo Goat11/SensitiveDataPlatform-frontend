e535e0d2e4bb98c23230a6f9fafb5c827d34970c
ting zhang <104016663+8280209zt@users.noreply.github.com>
2023-05-15 03:02:41 +0800

    Update AuditLogs.vue
    实现审计模块页面布局，尚未处理后端与接口

--- src/views/audit/AuditLogs.vue
+++ src/views/audit/AuditLogs.vue
@@ -1,13 +1,67 @@
 <template>
   <div>
-    AuditLogs page
+    <a-table :columns="columns" :data-source="data" :pagination="pagination"></a-table>
   </div>
 </template>
 
 <script>
+import { Table, Pagination } from 'ant-design-vue'
+import axios from 'axios'
+
 export default {
-    name: 'AuditLogs'
+  name: 'AuditLogs',
+  components: {
+    'a-table': Table,
+    'a-pagination': Pagination
+  },
+  data() {
+    return {
+      columns: [
+        { title: '序号', dataIndex: 'id', key: 'id' },
+        { title: '操作时间', dataIndex: 'operationTime', key: 'operationTime' },
+        { title: '操作者', dataIndex: 'operator', key: 'operator' },
+        { title: '操作模块名', dataIndex: 'moduleName', key: 'moduleName' },
+        { title: '条件', dataIndex: 'condition', key: 'condition' },
+        { title: '结果', dataIndex: 'result', key: 'result' },
+        { title: '操作', dataIndex: 'operation', key: 'operation' }
+      ],
+      data: [],
+      pagination: {
+        current: 1,
+        pageSize: 10,
+        total: 0,
+        showSizeChanger: true,
+        showQuickJumper: true,
+        pageSizeOptions: ['10', '20', '30', '40']
+      }
+    }
+  },
+  mounted() {
+    this.loadData()
+  },
+  methods: {
+    loadData() {
+      axios.get('/api/auditLog', {
+        params: {
+          pageNum: this.pagination.current,
+          pageSize: this.pagination.pageSize
+        }
+      }).then(response => {
+        const data = response.data.list
+        this.data = data
+        this.pagination.total = response.data.total
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    handlePageChange(page) {
+      this.pagination.current = page
+      this.loadData()
+    },
+    handlePageSizeChange(pageSize) {
+      this.pagination.pageSize = pageSize
+      this.loadData()
+    }
+  }
 }
 </script>
-
-<style scoped></style>
