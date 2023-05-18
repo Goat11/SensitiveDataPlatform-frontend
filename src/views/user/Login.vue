fe350cf705266489425fc0a3688dfd0b9358e4ad
ting zhang <104016663+8280209zt@users.noreply.github.com>
2023-05-18 19:58:42 +0800

    增加：登录页面、前后端耦合

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,70 +1,30 @@
 <template>
   <div class="main">
     <a-form id="formLogin" class="user-layout-login" ref="formLogin" :form="form" @submit="handleSubmit">
-      <a-tabs :activeKey="customActiveKey"
-              :tabBarStyle="{ textAlign: 'center', borderBottom: 'unset' }"
-              @change="handleTabClick">
+      <a-tabs :activeKey="customActiveKey" :tabBarStyle="{ textAlign: 'center', borderBottom: 'unset' }"
+        @change="handleTabClick">
         <!-- 账户密码登陆登陆 -->
         <a-tab-pane key="tab1" :tab="$t('user.login.tab-login-credentials')">
-          <a-alert v-if="isLoginError"
-                   type="error"
-                   showIcon
-                   style="margin-bottom: 24px;"
-                   :message="$t('user.login.message-invalid-credentials')" />
+          <a-alert v-if="isLoginError" type="error" showIcon style="margin-bottom: 24px;"
+            :message="$t('user.login.message-invalid-credentials')" />
           <a-form-item>
-            <a-input size="large"
-                     type="text"
-                     :placeholder="$t('user.login.username.placeholder')"
-                     v-decorator="[
-                       'username',
-                       { rules: [{ required: true, message: $t('user.userName.required') }, { validator: handleUsernameOrEmail }], validateTrigger: 'change' }
-                     ]">
+            <a-input size="large" type="text" :placeholder="$t('user.login.username.placeholder')" v-decorator="[
+              'username',
+              { rules: [{ required: true, message: $t('user.userName.required') }, { validator: handleUsernameOrEmail }], validateTrigger: 'change' }
+            ]">
               <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }" />
             </a-input>
           </a-form-item>
 
           <a-form-item>
-            <a-input-password size="large"
-                              :placeholder="$t('user.login.password.placeholder')"
-                              v-decorator="[
-                                'password',
-                                { rules: [{ required: true, message: $t('user.password.required') }], validateTrigger: 'blur' }
-                              ]">
+            <a-input-password size="large" :placeholder="$t('user.login.password.placeholder')" v-decorator="[
+              'password',
+              { rules: [{ required: true, message: $t('user.password.required') }], validateTrigger: 'blur' }
+            ]">
               <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }" />
             </a-input-password>
           </a-form-item>
         </a-tab-pane>
-        <!-- 手机登陆 -->
-        <!-- <a-tab-pane key="tab2" :tab="$t('user.login.tab-login-mobile')">
-          <a-form-item>
-            <a-input size="large"
-                     type="text"
-                     :placeholder="$t('user.login.mobile.placeholder')"
-                     v-decorator="['mobile', { rules: [{ required: true, pattern: /^1[34578]\d{9}$/, message: $t('user.login.mobile.placeholder') }], validateTrigger: 'change' }]">
-              <a-icon slot="prefix" type="mobile" :style="{ color: 'rgba(0,0,0,.25)' }" />
-            </a-input>
-          </a-form-item>
-
-          <a-row :gutter="16">
-            <a-col class="gutter-row" :span="16">
-              <a-form-item>
-                <a-input size="large"
-                         type="text"
-                         :placeholder="$t('user.login.mobile.verification-code.placeholder')"
-                         v-decorator="['captcha', { rules: [{ required: true, message: $t('user.verification-code.required') }], validateTrigger: 'blur' }]">
-                  <a-icon slot="prefix" type="mail" :style="{ color: 'rgba(0,0,0,.25)' }" />
-                </a-input>
-              </a-form-item>
-            </a-col>
-            <a-col class="gutter-row" :span="8">
-              <a-button class="getCaptcha"
-                        tabindex="-1"
-                        :disabled="state.smsSendBtn"
-                        @click.stop.prevent="getCaptcha"
-                        v-text="!state.smsSendBtn && $t('user.register.get-verification-code') || (state.time + ' s')"></a-button>
-            </a-col>
-          </a-row>
-        </a-tab-pane> -->
       </a-tabs>
 
       <a-form-item>
@@ -81,12 +41,8 @@
       </a-form-item>
 
       <a-form-item style="margin-top:24px">
-        <a-button size="large"
-                  type="primary"
-                  htmlType="submit"
-                  class="login-button"
-                  :loading="state.loginBtn"
-                  :disabled="state.loginBtn">{{ $t('user.login.login') }}</a-button>
+        <a-button size="large" type="primary" htmlType="submit" class="login-button" :loading="state.loginBtn"
+          :disabled="state.loginBtn">{{ $t('user.login.login') }}</a-button>
       </a-form-item>
 
       <div class="user-login-other">
@@ -104,15 +60,12 @@
       </div>
     </a-form>
 
-    <two-step-captcha v-if="requiredTwoStepCaptcha"
-                      :visible="stepCaptchaVisible"
-                      @success="stepCaptchaSuccess"
-                      @cancel="stepCaptchaCancel"></two-step-captcha>
+    <two-step-captcha v-if="requiredTwoStepCaptcha" :visible="stepCaptchaVisible" @success="stepCaptchaSuccess"
+      @cancel="stepCaptchaCancel"></two-step-captcha>
   </div>
 </template>
 
 <script>
-import md5 from 'md5'
 import TwoStepCaptcha from '@/components/tools/TwoStepCaptcha'
 import { mapActions } from 'vuex'
 import { timeFix } from '@/utils/util'
@@ -173,8 +126,7 @@ export default {
       const {
         form: { validateFields },
         state,
-        customActiveKey,
-        Login
+        customActiveKey
       } = this
 
       state.loginBtn = true
@@ -187,8 +139,11 @@ export default {
           const loginParams = { ...values }
           delete loginParams.username
           loginParams[!state.loginType ? 'email' : 'username'] = values.username
-          loginParams.password = md5(values.password)
-          Login(loginParams)
+          const tmp = {}
+          tmp['userID'] = loginParams.username
+          tmp['password'] = loginParams.password
+          // tmp['email'] = loginParams.email
+          this.Login(tmp)
             .then((res) => this.loginSuccess(res))
             .catch(err => this.requestFailed(err))
             .finally(() => {
@@ -245,6 +200,19 @@ export default {
       })
     },
     loginSuccess(res) {
+      if (res.status === 1) {
+        this.$router.push({ path: '/home' })
+        // 延迟 1 秒显示欢迎信息
+        setTimeout(() => {
+          this.$notification.success({
+            message: '欢迎',
+            description: `${timeFix()}，欢迎回来`
+          })
+        }, 1000)
+        this.isLoginError = false
+      } else {
+        this.requestFailed(res)
+      }
       console.log(res)
       // check res.homePage define, set $router.push name res.homePage
       // Why not enter onComplete
@@ -257,21 +225,12 @@ export default {
         })
       })
       */
-      this.$router.push({ path: '/' })
-      // 延迟 1 秒显示欢迎信息
-      setTimeout(() => {
-        this.$notification.success({
-          message: '欢迎',
-          description: `${timeFix()}，欢迎回来`
-        })
-      }, 1000)
-      this.isLoginError = false
     },
     requestFailed(err) {
       this.isLoginError = true
       this.$notification['error']({
         message: '错误',
-        description: ((err.response || {}).data || {}).message || '请求出现错误，请稍后再试',
+        description: ((err.response || {}).data || {}).msg || '请求出现错误，请稍后再试',
         duration: 4
       })
     }
